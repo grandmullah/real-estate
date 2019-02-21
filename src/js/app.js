d1f428dc659ad6863d2ceb1feeee992906e5e920
grandmullah <collinsbiegon9@gmail.com>
2019-02-21 10:10:03 +0300

    fixed

--- src/js/app.js
+++ src/js/app.js
@@ -63,6 +63,7 @@ App = {
         App.contracts.Interface.deployed().then(function(instance){
             return instance.Users(App.accounts);
         }).then(function(user){
+            console.log(user);
             $("#semail").html(user[1] );
             $("#sname").html(user[0]);
             $("#accadd").html(App.accounts);
